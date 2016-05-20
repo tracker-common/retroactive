25fbe2d8f219078b9d675da397a07b26c9a092dd
Espen Roth <eroth@mymail.mines.edu>
2016-05-20 11:32:29 -0600

    added webpack module and other stuff

---
+++ newproject/client/build-dev.sh
@@ -0,0 +1,10 @@
+rm build/index.html
+rm build/favicon.png
+rm build/app.css
+
+cp index.dev.html build/index.html
+cp favicon.png build/favicon.png
+cp app/app.css build/app.css
+
+
+webpack-dev-server --devtool eval --progress --colors --hot --content-base build
