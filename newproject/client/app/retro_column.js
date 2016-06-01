9afdd9161cf99137df3e5aa98e41e691f8d7f061
Espen Roth <eroth@mymail.mines.edu>
2016-06-01 12:58:06 -0600

    NOT COMPLETE - Adding modal for action items

--- newproject/client/app/retro_column.js
+++ newproject/client/app/retro_column.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import RetroItem from './retro_item'
 
+
 var RetroColumn = React.createClass({
+
+
 	//Props: Header Text, Items
 	render() {
-
+		var self = this;
 		var retroItems = this.props.items.map(function(item) {
 	      return (
-	        <RetroItem itemText={item}/>
+	        <RetroItem itemText={item} modalShow={self.modalShow} closeModal={self.closeModal} showModal={self.showModal}/>
 	      );
 	    });
 
@@ -16,22 +19,28 @@ var RetroColumn = React.createClass({
 				<h1 className="retro-columns__title">{this.props.HeaderText}</h1>
 				<div className="retro-column">			
 					<input type="text" placeholder="Type and hit enter to add..." onKeyPress={this.handleSubmit} ref="itemText"/>
-					<div className="retro-column__items">
+					<div className="retro-column-items">
 						{retroItems}
 					</div>
 				</div>
 			</div>
 			);
-		},
+	},
 
-		handleSubmit: function(e){
-			if (e.key === 'Enter') {
-				if(this.refs.itemText.value){
-      				this.props.handleAdd(this.props.columnId, this.refs.itemText.value);
-					this.refs.itemText.value = "";
-      			}
-			}
+	handleSubmit: function(e){
+		if (e.key === 'Enter') {
+			if(this.refs.itemText.value){
+  				this.props.handleAdd(this.props.columnId, this.refs.itemText.value);
+				this.refs.itemText.value = "";
+  			}
 		}
+	},
+	showModal: function(){
+		this.props.updateModalState(true);
+	},
+	closeModal: function(){
+		this.props.updateModalState(false);
+	}
 });
 
 export default RetroColumn;
