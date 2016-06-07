aa990797d40d493ef851092b4da4808b9705e182
Espen Roth <eroth@mymail.mines.edu>
2016-06-07 11:15:18 -0600

    action item status color

--- newproject/client/app/action_item.js
+++ newproject/client/app/action_item.js
@@ -4,18 +4,36 @@ var ActionItem = React.createClass({
 
 	getDefaultProps: function(){
 		return{ 
-			"status": "new",
+			"status": "Unscheduled",
 		};
 	},
 
 	render() {
+		var statusHtml = ""
+		switch(this.props.status){
+			case "Unscheduled":
+				statusHtml = (<span style={{color: '#88C1DE', fontWeight: 'bold'}}> Unscheduled </span>)
+				break;
+			case "Scheduled":
+				statusHtml = (<span style={{color: '#A7A7A7', fontWeight: 'bold'}}>Scheduled</span>)
+				break;
+			case "In Progress":			
+				statusHtml = (<span style={{color: '#DCD003', fontWeight: 'bold'}}>In Progress</span>)
+				break;
+			case "Rejected":			
+				statusHtml = (<span style={{color: '#F26373', fontWeight: 'bold'}}>Rejected</span>)
+				break;
+			case "Accepted":
+				statusHtml = (<span style={{color: '#72BF02', fontWeight: 'bold'}}>Accepted</span>)
+				break;
+		}
 		return (
 			<div className="retro-item">
 				<div className="retro-item-description">
 					{this.props.itemText}
 				</div>
 				<div className="status-and-action-bar">
-					<span className="item-info">Action Item Status: {this.props.status}</span>
+					<span className="item-info">Action Item Status: {statusHtml}</span>
 					<a className="edit-link link" onClick={this.show}>Edit</a>
 				</div>
 
